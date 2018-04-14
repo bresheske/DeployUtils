ff07f36788bf43484cc88f7bfd8ad6e404c69269
brandon <bresheske@gmail.com>
2018-04-14 09:08:43 -0500

    version 1.1.11.
    Added some func to the startService.

--- deployUtils/tools/startService.ts
+++ deployUtils/tools/startService.ts
@@ -2,12 +2,19 @@ import { write } from "./write";
 
 let exec = require('child_process').exec;
 
-export async function startService(name:string): Promise<boolean> {
+export async function startService(name:string, failonalreadystarted:boolean = false): Promise<boolean> {
     return new Promise<boolean>((res, rej) => {
         exec(`net start ${name}`, (err:string, stdout:string, stderr:string) => {
             if (err) {
-                write(`startService: Could not start "${name}". ${err}`);
-                res(false);
+                let r = /has already been started/;
+                if (r.test(stdout)) {
+                    write(`startService: Service "${name}" has already been started.`, null, false);
+                    res(!failonalreadystarted);
+                }
+                else {
+                    write(`startService: Could not start "${name}". ${err}`, null, false);
+                    res(false);
+                }
             }
             else if (stderr)
                 res(false);
